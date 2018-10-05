c337203d5f76c6a3b4d3aa1465e503c768c1f693
EduardoJW <35876133+EduardoJW@users.noreply.github.com>
2018-10-04 22:44:54 -0300

    adicionando assertivas para Vertice

--- Vertice.h
+++ Vertice.h
@@ -17,10 +17,40 @@ typedef struct Vertice vertice;
 
    } VER_tpCondRet ;
 
+/***********************************************************************
+*
+*  $FC Função:
+*	Cria Vertice
+*  $ED Descrição da função
+*       Aloca espaço e cria um Vertice incluindo seu nome valor e uma Lista de Antecessores e outra de Sucessores
+*  $EP Parâmetros
+*		nome - Nome do Vertice
+*		Valor - Valor inteiro do Vertice
+*		ListaAnt - Lista que guarda os Vertices anteriores
+*		ListaSuc - Lista que guarda os Vertices sucessores
+*
+*  $FV Valor retornado
+*	Retorna um ponteiro do tipo estruturado vertice
+*
+***********************************************************************/
 vertice* Cria_Vertice(char *nome, int Valor, LIS_tppLista ListaAnt,  LIS_tppLista ListaSuc);
 
-
-int setValorVertice(vertice*v,int Valor);
+/***********************************************************************
+*
+*  $FC Função:
+*	setValorVertice
+*  $ED Descrição da função
+*      Altera o valor do Vertice recebido
+*  $EP Parâmetros
+*		v - Vertice a ser alterado
+*		Valor - Novo valor do vertice
+*
+*  $FV Valor retornado
+*	 VER_CondRetOK se conseguiu alterar o valor do vertice
+*	  VER_CondRetVertVazio se o ponteiro de vertice fornecido esta vazio
+*
+***********************************************************************/
+VER_tpCondRet setValorVertice(vertice*v,int Valor);
 
 void Destroi_Vertice(vertice *v);
 
